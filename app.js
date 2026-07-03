63d927c12f4577f79789406660ecc2283916390b
gayatri-tonde <gayatritonde7@gmail.com>
2026-07-03 15:30:04 +0530

    feat: add minimatch and picocolors libraries with necessary files
    - Added minimatch library with package.json.
    - Introduced picocolors library including its README, LICENSE, and necessary JavaScript and TypeScript files.
    - Implemented terminal output formatting with ANSI colors in picocolors.
    - Created CSS styles for the navbar and footer components.
    - Added boilerplate layout for EJS views.
    - Developed edit listing page with form for updating listing details.

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const mongoose = require("mongoose");
 const port = 8080;
 const Listing = require("./models/listing");
 const path = require("path");
+const method_override = require("method-override");
+const ejsMate = require("ejs-mate");
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use(method_override("_method"));
 
 main()
   .then((res) => {
@@ -20,19 +28,43 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 
-app.get("/listing", async (req, res) => {
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+app.get("/listings", async (req, res) => {
   const allListing = await Listing.find({});
   res.render("listings/index.ejs", { allListing });
 });
 
-app.get("/listing/new", (req, res) => {
-  res.render("new.ejs");
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
 });
 
-app.get("/listing/:id", async (req, res) => {
+app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
+});
+
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.render("listings/show.ejs", { listing });
+});
+
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
 });
 
 app.get("/", () => {
